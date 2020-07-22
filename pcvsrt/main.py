481e1fe85660e08955532978d4dd1eb2da9a3a5e
Julien Adam <adamj@paratools.com>
2020-07-22 10:01:05 +0200

    MAIN: Start run() process

--- pcvsrt/main.py
+++ pcvsrt/main.py
@@ -1,47 +1,155 @@
 import os
-from pcvsrt.utils import logs
-from pcvsrt import config, profile
+import subprocess
+import yaml
+import shutil
+import tarfile
+import glob
+from tqdm import tqdm
+
+import pkg_resources
+import pcvsrt
+from pcvsrt.utils import logs, files
+from pcvsrt import config, profile, descriptor
 
 run_settings = {}
+list_of_tes = {}
 
 def __print_summary():
-    logs.print_section("Validation details")
+    global run_settings
+    logs.print_section("Validation details:")
+    logs.print_item("Build into: {}".format(run_settings['output']))
+    logs.print_item("Verbosity level: {}".format(run_settings['verbose']))
+    logs.print_item("Profile: '{}'".format(run_settings['pfname']))
+    logs.print_item("Paths:")
+    for k, v in run_settings['user_input'].items():
+        logs.print_item("{}: {}".format(k, v), depth=2)
+    # logs.print_item(": {}".format())
+
+
+def __build_jchronoss():
+    archive_name = None
+    archive_prefix = os.path.join(run_settings['output'], "cache/jchrns")
+    # FIXME: Dirty way to locate the archive
+    for f in glob.glob(os.path.join(pcvsrt.ROOTPATH, "../**/jchronoss-*"), recursive=True):
+        if 'jchronoss-' in f:
+            archive_name = os.path.join(pcvsrt.ROOTPATH, f)
+            break
+    assert(archive_name)
+    tarfile.open(os.path.join(archive_name)).extractall(archive_prefix)
+    jchronoss_root = glob.glob(os.path.join(archive_prefix, "*/CMakeLists.txt"))
+    assert (jchronoss_root)
+    with files.cwd(os.path.join(archive_prefix, "build")):
+        command = "cmake {} -DCMAKE_INSTALL_PREFIX={} -DENABLE_COLOR={} && make install".format(
+            os.path.dirname(jchronoss_root[0]),
+            os.path.join(archive_prefix, "install"),
+            "ON" if run_settings['color'] else "OFF"
+        )
+        res = subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
+
+def __setup_webview():
+    pass
 
 def __build_third_party():
     logs.print_section("Build third-party tools")
+
     logs.print_item("job orchestrator (JCHRONOSS)")
+    __build_jchronoss()
+
     logs.print_item("Web-based reporting tool")
+    __setup_webview()
 
 def prepare(settings):
     global run_settings
 
+    logs.print_section("Prepare environment")
     run_settings = settings
+
+    logs.print_item("Check valid output directory")
+    test_output_dir = os.path.join(run_settings['output'], "test_suite")
+    if os.path.isdir(test_output_dir):
+        if not run_settings['override']:
+            logs.err("Previous run artifacts found in {}. Please use '--override' to ignore.".format(settings['output']), abort=1)
+        else:
+            shutil.rmtree(test_output_dir)
+    os.mkdir(test_output_dir)
+
+    logs.print_item("Load Profile {}".format(settings['pfname']))
     pf = profile.Profile()
     run_settings['profile'] = pf.load(settings['pfname'])
-    __print_summary()
+    
+    logs.print_item("Save current configuration")
+    output_dir = settings['output']
+    with open(os.path.join(output_dir, "conf.yml"), 'w') as config_settings:
+        yaml_file = yaml.safe_dump(run_settings, config_settings)
+
     __build_third_party()
 
 
-def load_benchmarks(dirpaths):
-    logs.print_section("Load Benchmarks")
-    list_of_dirs = map(lambda x: os.path.abspath(x), dirpaths)
-    yaml_files = []
-    setup_files = []
-
-    for path in list_of_dirs:
-        for root, dirs, files in os.walk(path):
-            if '.pcvsrt' in root: # ignore pcvs-rt conf subdirs
-                continue
-            # save static YAML files (including *.in)
-            yaml_files += [os.path.join(root, f) for f in files if 'pcvs.yml' in f]
-            # save scripts
-            setup_files += [os.path.join(root, f) for f in files if f == 'pcvs.setup']
+def __substitute_yaml_tag(dict_to_parse):
     pass
 
 
+def load_benchmarks(test_dict):
+    global run_settings
+    global list_of_tes
+
+    logs.print_section("Load Benchmarks")
+    environ = {}
+
+    run_settings['user_input'] = test_dict
+    to_process = []
+    # discovery and processing split to add a progress bar (determinist)
+    for label, path in test_dict.items():
+        for root, dirs, files in os.walk(path):
+            if '.pcvsrt' in root or 'build_scripts' in root: # ignore pcvs-rt conf subdirs
+                continue
+           
+            to_process += [(path, root, f) for f in files if 'pcvs.setup' in f or 'pcvs.yml' in f]
+
+    for path, root, f in tqdm(to_process):
+        environ['pcvs_src'] = path
+        environ['pcvs_build'] = run_settings['output']
+        filepath = os.path.join(root, f)
+        te_package = root.replace(path, '').replace('/', ".")
+        fileroot = {}
+        print(f)
+        if f == 'pcvs.setup':
+            print(f)
+            res = subprocess.run(["echo", filepath, te_package], env=environ, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, shell=True)
+            print(res.stdout.decode("utf-8"))
+            try: pass
+                #fileroot = yaml.safe_load(res.stdout.decode('utf-8'))
+            except yaml.YAMLError as e:
+                logs.err("Bad YAML format:\n{}".format(e), abort=1)
+        elif 'pcvs.yml' in f:
+            try: pass
+                #fileroot = yaml.safe_load(open(filepath, 'r'))
+            except yaml.YAMLError as e:
+                logs.err("Bad YAML format:\n{}".format(e), abort=1)
+            if f.endswith('pcvs.yml.in'):
+                __substitute_yaml_tag(fileroot)
+                # apply replacement
+                pass
+        else:  # Not a pcvs-related file
+            continue
+
+        for blockname, blockdesc in fileroot.items():
+            te_name = label + te_package + "." + blockname
+            list_of_tes[te_name] = descriptor.TEDescriptor(fileroot)
+        print(list_of_tes)
+        logs.err("STOP")
+
+
 def run():
+    __print_summary()
+    logs.print_section("Start JCHRONOSS")
     pass
 
 
 def terminate():
+    logs.print_section("Build final archive")
+    if shutil.which("xsltproc") is not None:
+        logs.print_section("Build browsable results")
+
+
     pass
--- pcvsrt/scripts/run/commands.py
+++ pcvsrt/scripts/run/commands.py
@@ -11,6 +11,9 @@ from pcvsrt import main
 @click.option("-o", "--output", "output",
               default="./build", type=click.Path(exists=False),
               help="Where artefacts will be stored during/after the run")
+@click.option("-c", "--set-defaults", "set_default", 
+              default=False, is_flag=True,
+              help="Set default values for run options (WIP)")
 @click.option("-l", "--tee", "log",
               default=True, is_flag=True,
               help="Log the whole stdout/stderr")
@@ -20,27 +23,30 @@ from pcvsrt import main
 @click.option("--status", "status",
               default=None, is_flag=True,
               help="Display current run progression")
-@click.option("--pause", "pause",
+@click.option("-P", "--pause", "pause",
               default=None, is_flag=True,
               help="Pause the current run")
-@click.option("--resume", "resume",
+@click.option("-R", "--resume", "resume",
               default=None, is_flag=True,
               help="Resume a previously paused run")
-@click.option("--bootstrap", "bootstrap",
+@click.option("-b", "--bootstrap", "bootstrap",
               default=False, is_flag=True,
               help="Initialize basic test templates in given dirs")
-@click.argument("list_of_dirs", nargs=-1, type=click.Path(exists=True))
+@click.option("-f", "--override", "override",
+              default=False, is_flag=True,
+              help="Allow to reuse an already existing output dir")
+@click.argument("list_of_dirs", nargs=-1, type=str)
 @click.pass_context
 def run(ctx, profilename, output, log, detach, status,
-        resume, pause, bootstrap, list_of_dirs):
+        resume, pause, bootstrap, override, set_default, list_of_dirs):
 
+    # parse non-run situations
     if bootstrap:
         logs.info("Bootstrapping directories")
         for directory in list_of_dirs:
             run.init_structure(directory)
         exit(0)
-
-    if pause and resume:
+    elif pause and resume:
         logs.err("Cannot pause and resume the run at the same time!")
     elif pause:
         logs.info("Pause the current run")
@@ -51,20 +57,55 @@ def run(ctx, profilename, output, log, detach, status,
     elif status:
         logs.info("Get status about the running validation")
         exit(0)
+    elif set_default:
+        open_in_editor("defaults")
+        exit(0)
 
-    logs.banner()
-
+    # fill validation settings
     settings = {}
+    # for any 'None' value, a load from default should be made
     settings['verbose'] = ctx.obj['verbose']
     settings['color'] = ctx.obj['color']
     settings['pfname'] = profilename
-    settings['output'] = output
+    settings['output'] = os.path.join(os.path.abspath(output), ".pcvs")
     settings['tee'] = log
     settings['bg'] = detach
+    settings['override'] = override
 
+    # analys directory list
+    dict_of_dirs = {}
+    err_dirs = []
+    if not list_of_dirs:  # if not specified
+        curdir = os.getcwd()
+        dict_of_dirs[os.path.basename(curdir)] = curdir
+    else:  # once specified
+        for d in list_of_dirs:
+            if ':' in d:  # split under LABEL:PATH semantics
+                [label, testpath] = d.split(':')
+                testpath = os.path.abspath(testpath)
+            else: # otherwise, LABEL = dirname
+                testpath = os.path.abspath(d)
+                label = os.path.basename(testpath)
+
+            # if path does not exist
+            if not os.path.isdir(testpath):
+                err_dirs.append(testpath)
+            
+            dict_of_dirs[label] = testpath
+
+    # list all non-existent dirs
+    if err_dirs:
+        logs.err("Following arguments should be valid paths:")
+        for p in err_dirs:
+            logs.err('- {}'.format(p))
+        logs.err("please see '--help' for more information", abort=1)
+
+    logs.banner()
+    
     logs.print_header("pre-actions")
     main.prepare(settings)
-    main.load_benchmarks(list_of_dirs)
+
+    main.load_benchmarks(dict_of_dirs)
 
     logs.print_header("validation start")
     main.run()
