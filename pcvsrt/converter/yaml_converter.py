daf919760bdc030b10f36f3435f7c60eafb7c2bb
Julien Adam <adamj@paratools.com>
2020-07-31 12:44:08 +0200

    CONVERT: Fix iterators + rely on Click

--- pcvsrt/converter/yaml_converter.py
+++ pcvsrt/converter/yaml_converter.py
@@ -1,116 +1,189 @@
-import sys
-import os
 import json
-import yaml
-import functools
-import operator
+import os
 import pprint
 import re
+import sys
+
 import click
+import pkg_resources
+import yaml
 
-yaml.representer.ignore_aliases = lambda *data: True
+from pcvsrt import logs
 
-
-def abort(msg):
-    print(msg)
-    sys.exit(1)
-
-
-def get_by(data, klist):
-    return functools.reduce(operator.getitem, klist, data)
+desc_dict = dict()
 
 
-def set_with(data, klist, val):
+def separate_key_and_value(s: str, c: str) -> tuple:
+    """ helper to split the key and value from a string"""
+    array = s.split(c)
+    if len(array) > 1:
+        return (array[0], "".join(array[1:]))
+    else:
+        return (s, None)
+
+
+def set_with(data, klist, val, append=False):
+    """Add a value to a n-depth dict where the depth is declared as
+    a list of intermediate keys. the 'append' flag indicates if the given
+    'value' should be appended or replace the original content
+    """
+    # Just in case intermediate keys do not exist
     for key in klist[:-1]:
         data = data.setdefault(key, {})
-    data[klist[-1]] = val
+
+    # if the value should be appended
+    if append:
+        # if the key doe not exist, create the list
+        if klist[-1] not in data:
+            data[klist[-1]] = list()
+        # if it exists and is not a list ==> complain!
+        elif not isinstance(data[klist[-1]], list):
+            raise TypeError("fail")
+        # append the value
+        data[klist[-1]].append(val)
+    else:
+        # replace the value
+        data[klist[-1]] = val
 
 
-def flatten(dd, prefix=''):
-    return { prefix + "." + k if prefix else k : v 
-             for kk, vv in dd.items() 
-             for k, v in flatten(vv, kk).items() 
-             } if isinstance(dd, dict) else {prefix : dd} 
+def flatten(dd, prefix='') -> dict:
+    """ make the n-depth dict 'dd' a "flat" version, where the successive keys
+    are chained in a tuple. for instance:
+    {'a': {'b': {'c': value}}} --> {('a', 'b', 'c'): value}
+    """
+    return {prefix + "." + k if prefix else k: v
+            for kk, vv in dd.items()
+            for k, v in flatten(vv, kk).items()
+            } if isinstance(dd, dict) else {prefix: dd}
 
 
-static_fields = dict()
+def compute_new_key(k, m) -> str:
+    """replace in 'k' any pattern found in 'm'.
+    'k' is a string with placeholders, while 'm' is a match result with groups
+    named after placeholders.
+    This function will also expand the placeholder if 'call:' token is used to
+    execute python code on the fly (complex transformation)
+    """
+    replacement = ""
+    # basic replace the whole string with any placeholder
+    for elt in m.groupdict().keys():
+        k = k.replace("<"+elt+">", m.group(elt))
+
+    # if this key is a special python expression to process:
+    if k.startswith('call:'):
+        env = {'k': k, 'm': m}
+        # the 'k' & 'm' vars are exposed to evaluated code
+        code = compile(k.split("call:")[1], 'user-defined', 'exec')
+        eval(code, env)
+        # the 'k' is retrieved and used as a whole
+        replacement = env['k']
+    else:
+        replacement = k
+    return replacement
 
 
-def __process_new_key(k, m):
-    replacement = []
-    for elt in k.split("."):
-            
-        if elt.startswith("<") and elt.endswith(">"):
-            #tricky cases:
-            if '' in elt:
-                pass
-        
-            elt = elt[1:-1]
-            if elt in m.groupdict().keys():
-                replacement.append(m.group(elt))
-            else:
-                replacement.append(elt)
-        else:
-            replacement.append(elt)
-
-    return replacement if replacement else k
-
-def __match_input(key, ref_array):
+def check_if_key_matches(key, ref_array) -> tuple:
+    """list all matches for the current key in the new YAML description."""
+    # for each key to be replaced.
+    # WARNING: no order!
     for old_k, new_k in ref_array.items():
+        # compile the regex (useful ?)
         r = re.compile(old_k)
-        if re.search(r, key) is not None:
-            # print("{} == {}".format(old_k, key))    
+        if re.search(r, key) is not None:  # if the key exist
             # CAUTION: we only parse the first match_obj iteration:
-            # we do not consider a token to match multiple times !!!!  
+            # we do not consider a token to match multiple times in
+            # the source key!
             res = next(r.finditer(key))
+            # if there is a associated key in the new tree
             if new_k is not None:
                 if isinstance(new_k, list):
-                    dest_k = [ __process_new_key(i, res) for i in new_k]
+                    dest_k = [compute_new_key(i, res) for i in new_k]
                 else:
-                    dest_k = [__process_new_key(new_k, res)]
+                    dest_k = [compute_new_key(new_k, res)]
             else:
                 dest_k = []
             return (True, dest_k)
-        else:
+        else:  # the key does not exist
             pass
-    return (False, [])        
+    return (False, [])
 
 
-def process(data, ref_array=None, warn_if_missing=True):
+def process(data, ref_array=None, warn_if_missing=True) -> dict:
+    """Process YAML dict 'data' and return a transformed dict"""
     output = dict()
+
+    # desc_dict['second'] is set to contain all keys
+    # by opposition to desc_dict['first'] containing modifiers
     if not ref_array:
-        ref_array = static_fields['second']
+        ref_array = desc_dict['second']
+
+    # browse original data
     for k, v in data.items():
-        
-        # TBW
-        (valid, dest_k) = __match_input(k, ref_array)
-        
+        # if the node changed and should be moved, the tuple contains:
+        # - valid = node changed = key has been found in the desc.
+        # - dest_k = an array where each elt can be:
+        #    * the new key value
+        #    * the new key alongside with the transformed value as well
+        # in the latter case, a split is required to identify key & value
+        # an array is returned as a single node can produe multiple new nodes
+        (valid, dest_k) = check_if_key_matches(k, ref_array)
+
         if valid:
+            logs.info("Processing {}".format(k))
+            # An empty array means the key does not exist in the new tree.
+            # => discard
             if len(dest_k) <= 0:
                 continue
-            for elt_dest_k in dest_k:
-                set_with(output, elt_dest_k, v)
-        else:
-            if warn_if_missing:
-                print("missed {} !!".format(k))
-            set_with(output, k.split("."), v)
 
+            # Process each of the new keys
+            for elt_dest_k in dest_k:
+                (final_k, final_v, token) = (elt_dest_k, None, '')
+                # if a split is required
+                for token in ['+', '=']:
+                    (final_k, final_v) = separate_key_and_value(elt_dest_k,
+                                                                token)
+                    # the split() succeeded ? stop
+                    if final_v:
+                        break
+
+                # special case to handle the "+" operator to append a value
+                should_append = (token == '+')
+                # if none of the split() succeeded, just keep the old value
+                final_v = v if not final_v else final_v
+                # set the new key with the new value
+                set_with(output, final_k.split('.'), final_v, should_append)
+        else:
+            # warn when an old_key hasn't be declared in spec.
+            logs.info("DISCARDING {}".format(k))
+            if warn_if_missing:
+                logs.warn("Key {} undeclared in spec.".format(k))
+            # but still, keep it in the final output
+            set_with(output, k.split("."), v)
     return output
 
 
 def process_modifiers(data):
-    if "first" in static_fields.keys():
-        return process(data, static_fields["first"], warn_if_missing=False)
+    """applies rules in-place for the data dict.
+    Rules are present in the desc_dict['first'] sub-dict."""
+    if "first" in desc_dict.keys():
+        # do not warn for missing keys in that case (incomplete)
+        return process(data, desc_dict["first"], warn_if_missing=False)
     else:
         return data
 
-def __replace_token_with_re(tmp, refs):
+
+def replace_placeholder(tmp, refs) -> dict:
+    """
+    The given TMP should be a dict, where keys contain placeholders, wrapped
+    with "<>". Each placeholder will be replaced (i.e. key will be changed) by
+    the associated value in refs."""
+
     final = dict()
     for old, new in tmp.items():
         if old.startswith('__'):
             continue
 
-        replacement = [] 
+        replacement = []
         for elt in old.split('.'):
             insert = False
             for valid_k in refs.keys():
@@ -119,53 +192,102 @@ def __replace_token_with_re(tmp, refs):
                     replacement.append(elt.replace(valid_k, refs[valid_k]))
             if not insert:
                 replacement.append(re.escape(elt))
-        final["\.".join(replacement)] = new
+        final[r"\.".join(replacement)] = new
     return final
 
 
+def print_version(ctx, param, value) -> None:
+    """print converter version number, tied to PCVS-RT version number """
+    if not value or ctx.resilient_parsing:
+        return
+    version = pkg_resources.require("pcvs-rt")[0].version
+    click.echo('YAML Converter (pcvs-rt Suite) -- version {}'.format(version))
+    ctx.exit()
 
-@click.command("pcvs_convert", help="Tool converting old to new syntax")
-@click.option("-k", "--kind", "kind", required=True, type=click.Choice(['compiler', 'runtime', 'environment', 'te'], case_sensitive=False))
-@click.option("-s", "--scheme", "scheme", type=click.File(), required=False, default=None)
-@click.argument("input_file", type=click.File())
-def main(kind, input_file, scheme):
 
+@click.command("pcvs_convert", short_help="YAML to YAML converter")
+@click.option("-k", "--kind", "kind",
+              type=click.Choice(['compiler', 'runtime', 'environment', 'te'],
+                                case_sensitive=False),
+              required=True, help="Select a kind to apply for the file")
+@click.option("-s", "--scheme", "scheme", required=False, default=None,
+              type=click.Path(exists=True, dir_okay=False, readable=True),
+              help="Override default spec by custom one")
+@click.option("-v", "--verbose", count=True,
+              help="Enable up to 3-level log messages")
+@click.option("-V", "--version", expose_value=False, callback=print_version,
+              is_eager=True, help="Print version", is_flag=True)
+@click.option("-c", "--color/--no-color", "color",
+              default=True, is_flag=True, show_envvar=True,
+              help="Use colors to beautify the output")
+@click.option("-g", "--glyph/--no-glyph", "encoding",
+              default=True, is_flag=True, show_envvar=True,
+              help="enable/disable Unicode glyphs")
+@click.option("-o", "--output", "out", default=None,
+              type=click.Path(exists=False, dir_okay=False),
+              help="Filepath where to put the converted YAML")
+@click.argument("input_file", type=click.Path(exists=True, dir_okay=False, 
+                                              readable=True, allow_dash=True))
+@click.pass_context
+def main(ctx, color, encoding, verbose, kind, input_file, out, scheme) -> None:
+    """
+    Process the conversion from one YAML format to another.
+    Conversion specifications are described by the SCHEME file.
+    """
+    # Click specific-related
+    ctx.color = color
+    
+    logs.init(verbose, encoding)
+    logs.print_header("YAML Conversion")
+    logs.print_item("Loading data file & scheme")
+    # load the input file
+    f = sys.stdin if input_file == '-' else open(input_file, 'r')
+    data_to_convert = yaml.load(f, Loader=yaml.FullLoader)
+
+    # load the scheme
     if not scheme:
-        scheme = os.path.join(os.path.dirname(__file__), "convert.json")
-    
-    data_to_convert = yaml.load(input_file.read(), Loader=yaml.FullLoader)
-    
-    with open(scheme, 'r') as f:
-        tmp = json.load(f)
-    
-    if '__modifiers' in tmp.keys():
-        static_fields['first'] = __replace_token_with_re(tmp['__modifiers'], tmp['__tokens'])
-    static_fields['second'] = __replace_token_with_re(tmp, tmp['__tokens'])
+        scheme = open(os.path.join(os.path.dirname(__file__), "convert.json"))
+    tmp = json.load(scheme)
 
-    # first, "flattening"
+    # if modifiers are declared, replace token with regexes
+    if '__modifiers' in tmp.keys():
+        desc_dict['first'] = replace_placeholder(tmp['__modifiers'],
+                                                 tmp['__tokens'])
+    desc_dict['second'] = replace_placeholder(tmp,
+                                              tmp['__tokens'])
+
+    logs.info("Conversion list {old_key -> new_key):",
+              "{}".format(pprint.pformat(desc_dict)))
+
+    # first, "flattening" the original array: {(1, 2, 3): "val"}
     data_to_convert = flatten(data_to_convert, kind)
+
+    # then, process modifiers, altering original data before processing
+    logs.print_item("Process alterations to the original data")
     data_to_convert = process_modifiers(data_to_convert)
     # as modifiers may have created nested dictionaries:
     # => "flattening" again, but with no prefix (persistent from first)
     data_to_convert = flatten(data_to_convert, "")
-    final_layout = process(data_to_convert)
 
-    prefix, base = os.path.split(input_file.name)
-    with open(os.path.join(prefix, "new-"+base), "w") as f:
-        yaml.dump(final_layout, f)
+    # Finally, convert the original data to the final yaml dict
+    logs.print_item("Process the data file: {}".format(
+        "standard input" if input_file == "-" else input_file))
+    final_data = process(data_to_convert)
+    logs.info("Final layout:", "{}".format(pprint.pformat(final_data)))
 
-    pprint.pprint(final_layout)
-    
-    
+    if out is None:
+        prefix, base = os.path.split(
+            "./file.yml" if input_file == "-" else input_file)
+        out = os.path.join(prefix, "converted-"+base)
 
+    with open(out, "w") as f:
+        logs.print_section("Converted file written into: {}".format(f.name))
+        yaml.dump(final_data, f)
 
 
 """
 MISSING:
-- group
 - compiler.package_manager
 - runtime.package_manager
-- runtime.iterators
 - te.package_manager
-- herit* become flat
 """
