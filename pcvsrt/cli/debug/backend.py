641e7a7042cd2edd5f50279930e6c168692dd13f
Julien Adam <adamj@paratools.com>
2020-10-05 11:18:38 +0200

    MAIN: Light pcvs debug for test-names

--- pcvsrt/cli/debug/backend.py
+++ pcvsrt/cli/debug/backend.py
@@ -1,7 +1,16 @@
 import os
-import glob
-
 import yaml
 
 from pcvsrt.helpers import io, log
 
+
+def retrieve_test_script(testname):
+    # first one is directory name
+    # last one is test name
+    prefix = testname.split('.')[:-1]
+    return os.path.join(
+        "./.pcvs/test_suite/",
+        "/".join(prefix),
+        "list_of_tests.sh"
+    )
+    
