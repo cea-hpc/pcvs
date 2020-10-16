84cf1a15274a86ae3311967f84384c043ea5c3dc
Julien Adam <adamj@paratools.com>
2020-10-16 08:39:06 +0200

    DOC: bank

--- pcvsrt/cli/bank/backend.py
+++ pcvsrt/cli/bank/backend.py
@@ -13,20 +13,26 @@ BANK_STORAGE=""
 
 
 def init():
+    """Called when program initializes. Detects defined banks in
+    $USER_STORAGE/banks.yml
+    """
     global BANKS, BANK_STORAGE
     BANK_STORAGE = os.path.join(io.STORAGES['user'], "saves/banks.yml")
     try:
         with open(BANK_STORAGE, 'r') as f:
             BANKS = yaml.load(f, Loader=yaml.FullLoader)
     except FileNotFoundError:
+        # nothing to do, file may not exist
         pass
 
 
 def list_banks():
+    """Accessor to bank dict (outside of this module)"""
     return BANKS
 
 
 def flush_to_disk():
+    """Save in-memory bank management to disk. This only implies 'banks.yml'"""
     global BANKS, BANK_STORAGE
     try:
         with open(BANK_STORAGE, 'w') as f:
@@ -36,25 +42,32 @@ def flush_to_disk():
 
 
 class Bank:
+    """A 'Bank' object manages persistent data between run(s) and test reports.
+    A bank is initialized at a given mount point on the filesystem and stores
+    data in it, following a label-based tree.
+    """
     def __init__(self, name, bank_path=None):
         self._name = name
         self._data = Dict()
         self._datafile = None
 
+        # If the bank 'path' is not defined but known from global config:
         if bank_path is None and name in BANKS.keys():
             self._path = BANKS[self._name]
         else:
             self._path = str(bank_path)
         
+        # attempt to load file management configuration stored into the bank
         self._datafile = os.path.join(self._path, "data.yml")
         if os.path.isfile(self._datafile):
-            #try:
-            with open(self._datafile, 'r') as fh:
-                self._data = Dict(yaml.load(fh, Loader=yaml.FullLoader))
-            #except yaml.YAMLError:
-            #    log.err("Error while loading bank data file")
+            try:
+                with open(self._datafile, 'r') as fh:
+                    self._data = Dict(yaml.load(fh, Loader=yaml.FullLoader))
+            except yaml.YAMLError:
+                log.err("Error while loading bank data file")
 
     def flush(self):
+        """save the current bank into its own 'data.yml' file"""
         if os.path.isfile(self._datafile):
             try:
                 with open(self._datafile, 'w') as fh:
@@ -63,6 +76,7 @@ class Bank:
                 log.err("Error while saving bank data file")
 
     def register(self):
+        """create a new bank and save it on disk"""
         global BANKS
         BANKS[self._name] = self._path
         try:
@@ -71,6 +85,9 @@ class Bank:
             log.err('Registering a dir w/ existing data.yml ?')
     
     def unregister(self):
+        """delete a previously registered bank.
+        Note this won't delete the directory itself but any PCVS relatives
+        """
         global BANKS
         BANKS.pop(self._name, None)
         try:
@@ -79,10 +96,12 @@ class Bank:
             pass
 
     def exists(self):
+        """check if current bank is actually registered into global management"""
         global BANKS
         return len([i for i in BANKS.keys() if i == self._name]) == 1
 
     def save(self, k, v):
+        """store a new data (v) into the current bank, labeled 'k' """
         # for now, only support archives
         if not os.path.isfile(v):
             log.err("Banks only support file submission (for now)")
@@ -96,7 +115,7 @@ class Bank:
         self.flush()
 
     def load(self, k, dest=None):
-        
+        """load something (v) from the current bank, under label 'k' """
         if dest is None:
             dest = os.getcwd()
         elif not os.path.exists(dest):
@@ -105,11 +124,13 @@ class Bank:
         if k not in self._data:
             log.err("No key named '{}'".format(k))
         
+        # copy full content
         for elt in self._data[k]:
             shutil.copy(os.path.join(self._path, k, elt),
                         os.path.join(dest, os.path.basename(elt)))
 
     def delete(self, k):
+        """Delete data from a the current bank"""
         if k not in self._data:
             log.err("No key named '{}'".format(k))
         
@@ -118,6 +139,7 @@ class Bank:
         self.flush()
     
     def show(self):
+        """List bank's content"""
         log.print_section('Path: {}'.format(self._path))
         for k, v in self._data.items():
             log.print_section("{}:".format(k))
--- pcvsrt/cli/bank/commands.py
+++ pcvsrt/cli/bank/commands.py
@@ -6,6 +6,7 @@ from pcvsrt.helpers import log, io
 
 
 def compl_list_banks(ctx, args, incomplete):
+    """bank name completion function"""
     pvBank.init()
     array = list()
     for k, v in pvBank.BANKS.items():
@@ -13,29 +14,30 @@ def compl_list_banks(ctx, args, incomplete):
     return [elt for elt in array if incomplete in elt[0]]
 
 
-@click.group(name="bank", short_help="Manage validation repositories")
+@click.group(name="bank", short_help="Persistent data repository management")
 @click.pass_context
 def bank(ctx):
+    """root function to handle a bank."""
     pass
 
 
-@bank.command(name="list", short_help="List available repositories")
-@click.argument("name", nargs=1, required=False, type=str,
-                autocompletion=compl_list_banks)
+@bank.command(name="list", short_help="List known repositories")
 @click.pass_context
-def bank_list(ctx, name):
+def bank_list(ctx):
+    """
+    List known repositories, stored under $HOME_STORAGE/banks.yml"""
     log.print_header("Bank View")
     for label, path in pvBank.list_banks().items():
         log.print_item("{: <8s}: {}".format(label.upper(), path))
 
 
-@bank.command(name="show", short_help="Display bank's content")
+@bank.command(name="show", short_help="Display data stored in a repo.")
 @click.argument("name", nargs=1, required=True, type=str, autocompletion=compl_list_banks)
 @click.pass_context
 def bank_show(ctx, name):
-
+    """Display all data stored into NAME repository"""
     log.print_header("Bank View")
-    
+
     b = pvBank.Bank(name)
     if not b.exists():
         log.err("'{}' does not exist".format(name))
@@ -43,13 +45,12 @@ def bank_show(ctx, name):
         b.show()
 
 
-
 @bank.command(name="create", short_help="Register a new bank")
 @click.argument("name", nargs=1, required=True, type=str, autocompletion=compl_list_banks)
 @click.argument("path", nargs=1, type=click.Path(exists=True, file_okay=False), required=True)
 @click.pass_context
 def bank_create(ctx, name, path):
-
+    """Create a new bank, named NAME, data will be stored under PATH."""
     log.print_header("Bank View")
     if path is None:
         path = os.getcwd()
@@ -63,7 +64,7 @@ def bank_create(ctx, name, path):
         b.register()
         pvBank.flush_to_disk()
 
-@bank.command(name="destroy", short_help="Register a new bank")
+@bank.command(name="destroy", short_help="Delete an existing bank")
 @click.argument("name", nargs=1, required=True, type=str, autocompletion=compl_list_banks)
 @click.confirmation_option(
     "-f", "--force", "force",
@@ -71,7 +72,10 @@ def bank_create(ctx, name, path):
     help="Do not ask for confirmation before deletion")
 @click.pass_context
 def bank_destroy(ctx, name):
-
+    """Remove the bank NAME from PCVS management. This does not include
+    repository deletion. 'data.yml' and bank entry in the configuratino file
+    will be removed but existing data are preserved.
+    """
     log.print_header("Bank View")
     b = pvBank.Bank(name)
     if not b.exists():
@@ -81,12 +85,14 @@ def bank_destroy(ctx, name):
         pvBank.flush_to_disk()
 
 
-@bank.command(name="save", short_help="Save a new object in datastore")
+@bank.command(name="save", short_help="Store an object to the datastore")
 @click.argument("name", nargs=1, required=True, type=str, autocompletion=compl_list_banks)
 @click.argument("attr", nargs=1, required=True)
 @click.argument("obj", nargs=1, required=True)
 @click.pass_context
 def bank_save_content(ctx, name, attr, obj):
+    """Save an object OBJ (currently only filepaths) under ATTR label into the
+    previously-registered bank NAME"""
     bank = pvBank.Bank(name)
     if not bank.exists():
         log.err("Unable to save content into a non-existent bank.",
@@ -103,6 +109,8 @@ def bank_save_content(ctx, name, attr, obj):
               help="Directory where extracting saved objects")
 @click.pass_context
 def bank_load_content(ctx, name, attr, dest):
+    """Load  any object under ATTR label from the previously-registered bank
+    NAME"""
     bank = pvBank.Bank(name)
     if not bank.exists():
         log.err("Unable to load content from a non-existent bank.",
@@ -120,10 +128,10 @@ def bank_load_content(ctx, name, attr, dest):
     help="Do not ask for confirmation before deletion")
 @click.pass_context
 def bank_delete_content(ctx, name, attr):
+    """Delete anyobject under ATTR label from the previously-registered bank NAME"""
     bank = pvBank.Bank(name)
     if not bank.exists():
         log.err("Unable to modify content from a non-existent bank.",
                 "Please use the 'create' command first.")
     else:
         bank.delete(attr)
-        
