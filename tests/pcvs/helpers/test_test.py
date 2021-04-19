3805d266c696b1301c347de08ff4feffbc94ea28
Paul Canat <pcanat@paratools.fr>
2021-04-19 14:46:41 +0200

    TESTS: Test class

--- tests/pcvs/helpers/test_test.py
+++ tests/pcvs/helpers/test_test.py
@@ -5,13 +5,57 @@ import pytest
 
 from pcvs.helpers import test as tested
 
-
+from pcvs import PATH_INSTDIR, NAME_BUILDIR, NAME_SRCDIR
+from pcvs.helpers import log
 def legacy_yaml_file():
     assert(False)
 
+log.init()
 
 def load_yaml_file():
     assert(False)
+
+def test_load_yaml_file():
+    log.manager.print(NAME_BUILDIR)
+    tested.load_yaml_file(os.path.join(PATH_INSTDIR, "schemes/criterion-scheme.yml"), 
+        os.path.join(PATH_INSTDIR, "schemes"), 
+        NAME_BUILDIR, 
+        "schemes")
+
+    # test invalid yml ?
+
+    # tested.load_yaml_file(os.path.join(os.path.dirname(__file__), "appveyor.yml"), 
+    #     os.path.join(PATH_INSTDIR, "schemes"), 
+    #     NAME_BUILDIR, 
+    #     "schemes")
+
+def test_TestFile():
+    pass
+    # testfile = tested.TestFile(os.path.join(PATH_INSTDIR, "schemes/criterion-scheme.yml"), os.path.dirname(__file__), label="test", prefix="schemes")
+    # testfile.process()
+
+def test_Test():
+    test = tested.Test(name = "testname", 
+        command = "testcommand", 
+        nb_res = "testdim",
+        te_name = "testte_name",
+        subtree = "testsubtree",
+        chdir = "testchdir",
+        dep = "testdep")
+    assert(test.name == "testname")
+    assert(test.command == "testcommand")
+    test.override_cmd("testnewcommand")
+    assert(test.command == "testnewcommand")
+    assert(test.get_dim() == "testdim")
+    assert(test.been_executed() == False)
+    assert(test.state == tested.Test.STATE_NOT_EXECUTED)
+    test.executed()
+    assert(test.been_executed() == True)
+    testjson = test.to_json()
+    assert(testjson["id"]["te_name"] == "testte_name")
+    assert(testjson["id"]["subtree"] == "testsubtree")
+    assert(testjson["id"]["full_name"] == "testname")
+
 
 @patch.dict(os.environ, {'HOME': '/home/user', 'USER': 'superuser'})
 def test_replace_tokens():
