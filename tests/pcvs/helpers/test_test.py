28db49f358385c798a3f1ee0524afacc31ad5f9d
Paul Canat <pcanat@paratools.fr>
2021-04-26 14:33:37 +0200

    TESTS: 'Test' + 'TestFile' + 'TEDescriptor' classes (TEDescriptor work in progress)

--- tests/pcvs/helpers/test_test.py
+++ tests/pcvs/helpers/test_test.py
@@ -2,11 +2,19 @@ import os
 from unittest.mock import patch
 
 import pytest
-
+import pcvs
 from pcvs.helpers import test as tested
 
 from pcvs import PATH_INSTDIR, NAME_BUILDIR, NAME_SRCDIR
 from pcvs.helpers import log
+from pcvs.helpers import system
+from unittest.mock import patch, Mock
+from addict import Dict
+from click.testing import CliRunner
+import yaml
+from pcvs.helpers import utils
+from pcvs.helpers import exceptions
+
 def legacy_yaml_file():
     assert(False)
 
@@ -17,7 +25,6 @@ def load_yaml_file():
 
 @patch.dict(os.environ, {'HOME': '/home/user', 'USER': 'superuser'})
 def test_load_yaml_file():
-    log.manager.print(NAME_BUILDIR)
     tested.load_yaml_file(os.path.join(PATH_INSTDIR, "schemes/criterion-scheme.yml"), 
         os.path.join(PATH_INSTDIR, "schemes"), 
         NAME_BUILDIR, 
@@ -30,10 +37,61 @@ def test_load_yaml_file():
     #     NAME_BUILDIR, 
     #     "schemes")
 
-def test_TestFile():
-    pass
-    # testfile = tested.TestFile(os.path.join(PATH_INSTDIR, "schemes/criterion-scheme.yml"), os.path.dirname(__file__), label="test", prefix="schemes")
-    # testfile.process()
+@pytest.fixture
+def isolated_yml_test():
+    testyml = {
+        "test_MPI_2INT":{
+            "build":{
+                "cflags": "-DSYMB=MPI_2INT -DTYPE1='int' -DTYPE='int'",
+                "files": "'@SRCPATH@/constant.c'",
+                "sources": {
+                    "binary": "test_MPI_2INT"
+                }
+            },
+            "group": "GRPSERIAL",
+            "run":{
+                "program": "test_MPI_2INT"
+            },
+            "tag": [
+                "std_1",
+                "constant"
+            ]
+        }
+    }
+    with CliRunner().isolated_filesystem():
+        path = os.getcwd()
+        testdir = "test-dir"
+        os.makedirs(testdir)
+        with open(os.path.join(path, testdir, "pcvs.yml"), "w") as fh:
+            fh.write(yaml.safe_dump(testyml))
+        yield path
+    # utils.delete_folder(testdir)
+
+
+@patch("pcvs.helpers.system.MetaConfig.root", system.MetaConfig({
+    "__internal": {
+        "cc_pm": "test_cc_pm"
+    },
+    "validation": {
+        "output": "test_output",
+        "dirs": {
+            "keytestdir": "valuetestdir"
+        }
+    }
+}))
+@patch("pcvs.helpers.test.TEDescriptor")
+def test_TestFile(tedesc, isolated_yml_test):
+    def dummydesc(obj):
+        pass
+    tedesc.construct_tests = dummydesc
+    l = log.IOManager(logfile = "out.log")
+    testfile = tested.TestFile(os.path.join(isolated_yml_test, "test-dir/pcvs.yml"), 
+        os.path.dirname(__file__), 
+        label="keytestdir", 
+        prefix=".")
+    testfile.process()
+    testfile.generate_debug_info()
+    testfile.flush_sh_file()
 
 def test_Test():
     test = tested.Test(name = "testname", 
@@ -42,7 +100,10 @@ def test_Test():
         te_name = "testte_name",
         subtree = "testsubtree",
         chdir = "testchdir",
-        dep = "testdep")
+        dep = "testdep",
+        env = ["testenv"],
+        matchers = {"matcher1": {"expr": "test"}},
+        valscript = "testvalscript",)
     assert(test.name == "testname")
     assert(test.command == "testcommand")
     test.override_cmd("testnewcommand")
@@ -56,7 +117,56 @@ def test_Test():
     assert(testjson["id"]["te_name"] == "testte_name")
     assert(testjson["id"]["subtree"] == "testsubtree")
     assert(testjson["id"]["full_name"] == "testname")
+    assert(test.strstate == "OTHER")
+    test.save_final_result()
+    test.generate_script()
 
+@patch("pcvs.helpers.system.MetaConfig.root", system.MetaConfig({
+    "__internal": {
+        "cc_pm": "test_cc_pm"
+    },
+    "validation": {
+        "output": "test_output",
+        "dirs": {
+            "keytestdir": "valuetestdir"
+        }
+    },
+    "group": {
+        "GRPSERIAL": {}
+    },
+    "criterion": {}
+}))
+def test_TEDescriptor(isolated_yml_test):
+    tested.TEDescriptor.init_system_wide("n_node")
+    node = {
+            "build":{
+                "cflags": "-DSYMB=MPI_2INT -DTYPE1='int' -DTYPE='int'",
+                "files": "'@SRCPATH@/constant.c'",
+                "sources": {
+                    "binary": "test_MPI_2INT"
+                }
+            },
+            "group": "GRPSERIAL",
+            "run": {
+                "program": "test_MPI_2INT"
+            },
+            "tag": [
+                "std_1",
+                "constant"
+            ]
+        }
+    tedesc = tested.TEDescriptor("foo", 
+        node,
+        "keytestdir", 
+        "bar")
+    # for i in tedesc.construct_tests():
+    #     print(i)
+    # raise Exception
+    with pytest.raises(exceptions.TestException.TDFormatError):
+        tested.TEDescriptor("foo", 
+            "StrInsteadOfDict",
+            "keytestdir", 
+            "bar")
 
 @patch.dict(os.environ, {'HOME': '/home/user', 'USER': 'superuser'})
 def test_replace_tokens():
