92f92314aec5fe37af04fc8210cfce14e2a5e368
Julien Adam <adamj@paratools.com>
2021-04-26 10:12:27 +0200

    MAIN: global vars linking conf.yml & rawdata/

--- pcvs/__init__.py
+++ pcvs/__init__.py
@@ -5,6 +5,8 @@ import click
 NAME_BUILDIR = ".pcvs-build"
 NAME_BUILDFILE = ".pcvs-isbuilddir"
 NAME_SRCDIR = ".pcvs"
+NAME_BUILD_CONF_FN = "conf.yml"
+NAME_BUILD_RESDIR = "rawdata"
 
 PATH_INSTDIR = os.path.dirname(__file__)
 PATH_HOMEDIR = click.get_app_dir('pcvs', force_posix=True)
