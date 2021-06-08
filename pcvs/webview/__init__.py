9506455ddaf325025c07de45b7ada3d6388fed66
Paul Canat <pcanat@paratools.fr>
2021-06-08 17:33:36 +0200

    FLASK: adding real time webview for list and detailed list

--- pcvs/webview/__init__.py
+++ pcvs/webview/__init__.py
@@ -16,6 +16,21 @@ def create_app(global_tree=None, test_config=None):
     :return: the application
     :rtype: :class:`Flask`
     """
+    global_tree_recv = {
+        "metadata": {
+            "rootdir": "remote server",
+            "count":{
+                "tests": 0,
+                "labels": 0,
+                "tags": 0,
+                "files": 0
+            }
+        },
+        "label": {},
+        "tag": {},
+        "status": {}
+    }
+
     app = Flask(__name__, template_folder=os.path.join(
         PATH_INSTDIR, "webview/templates"))
 
@@ -47,11 +62,6 @@ def create_app(global_tree=None, test_config=None):
         :return: webpage content
         :rtype: str
         """
-        if 'json' in request.args.get('render', []):
-            return jsonify({"tag": global_tree["metadata"]["count"]["tags"],
-                            "label": global_tree["metadata"]["count"]["labels"],
-                            "test": global_tree["metadata"]["count"]["tests"],
-                            "files": global_tree["metadata"]["count"]["files"]})
         return render_template('main.html',
                                rootdir=global_tree['metadata']['rootdir'],
                                nb_tests=global_tree['metadata']['count']['tests'],
@@ -67,7 +77,17 @@ def create_app(global_tree=None, test_config=None):
         :return: webpage content
         :rtype: str
         """
-        return render_template('tbw.html')
+        if 'json' in request.args.get('render', []):
+            return jsonify({"tag": global_tree_recv["metadata"]["count"]["tags"],
+                            "label": global_tree_recv["metadata"]["count"]["labels"],
+                            "test": global_tree_recv["metadata"]["count"]["tests"],
+                            "files": global_tree_recv["metadata"]["count"]["files"]})
+        return render_template('realtime.html',
+                               rootdir=global_tree_recv['metadata']['rootdir'],
+                               nb_tests=global_tree_recv['metadata']['count']['tests'],
+                               nb_labels=global_tree_recv['metadata']['count']['labels'],
+                               nb_tags=global_tree_recv['metadata']['count']['tags'],
+                               nb_files=global_tree_recv['metadata']['count']['files'])
 
     @app.route('/compare')
     def compare():
@@ -105,6 +125,33 @@ def create_app(global_tree=None, test_config=None):
             })
         return jsonify(out)
 
+    @app.route("/realtime/<selection>/list")
+    def get_list_realtime(selection):
+        """Get a listing.
+
+        The response will depend on the request, which can be:
+            * tag
+            * label
+            * status
+
+        Providing a GET ``render`` to ``json`` returns the raw JSON version.
+
+        :param selection: which listing to target
+        :type selection: str
+        :return: web content
+        :rtype: str
+        """
+        if 'json' not in request.args.get('render', []):
+            return render_template('list_view_realtime.html', selection=selection)
+
+        out = list()
+        for name, value in global_tree_recv[selection].items():
+            out.append({
+                "name": name,
+                "count": value['metadata']['count']
+            })
+        return jsonify(out)
+
     @app.route("/<selection>/detail")
     def get_details(selection):
         """Get a detailed view of a component.
@@ -135,7 +182,38 @@ def create_app(global_tree=None, test_config=None):
         if request_item in global_tree[selection].keys():
             for test in global_tree[selection][request_item]['tests']:
                 out.append(test)
-        printjson(global_tree)
+        return jsonify(out)
+
+    @app.route("/realtime/<selection>/detail")
+    def get_details_realtime(selection):
+        """Get a detailed view of a component.
+
+        The response will depend on the request, which can be:
+            * tag
+            * label
+            * status
+
+        Providing a GET ``render`` to ``json`` returns the raw JSON version.
+
+        :param selection: which view to target
+        :type selection: str
+        :return: web response
+        :rtype: str
+        """
+        out = list()
+        request_item = request.args.get('name', None)
+
+        if selection not in global_tree.keys():
+            abort(404, description="{} is not a valid selection!".format(selection))
+
+        if 'json' not in request.args.get('render', []):
+            return render_template("detailed_view_realtime.html",
+                                   selection=selection,
+                                   sel_item=request_item)
+
+        if request_item in global_tree_recv[selection].keys():
+            for test in global_tree_recv[selection][request_item]['tests']:
+                out.append(test)
         return jsonify(out)
 
     @app.route("/submit", methods=["POST", "GET"])
@@ -147,16 +225,16 @@ def create_app(global_tree=None, test_config=None):
             addToGlobalTree(to_add, "label", [to_add["test_data"]["id"]["label"]])
             addToGlobalTree(to_add, "tag", to_add["test_data"]["data"]["tags"])
             addToGlobalTree(to_add, "status", [to_add["state"]])
-            global_tree["metadata"] = to_add["metadata"]
+            global_tree_recv["metadata"] = to_add["metadata"]
             return "This page is not meant to be visited"
 
     def addToGlobalTree(to_add, sorting_type, names):
-        if sorting_type not in global_tree:
-            global_tree["sorting_type"] = {}
+        if sorting_type not in global_tree_recv:
+            global_tree_recv["sorting_type"] = {}
         for name in names:
-            if name not in global_tree[sorting_type]:
-                global_tree[sorting_type][name] = {"tests": []}
-                global_tree[sorting_type][name]["metadata"] = {
+            if name not in global_tree_recv[sorting_type]:
+                global_tree_recv[sorting_type][name] = {"tests": []}
+                global_tree_recv[sorting_type][name]["metadata"] = {
                     "count": {
                         "WAITING": 0,
                         "IN_PROGRESS": 0,
@@ -167,9 +245,9 @@ def create_app(global_tree=None, test_config=None):
                         "total": 0
                     }
                 }
-            global_tree[sorting_type][name]["tests"].append(to_add["test_data"])
-            global_tree[sorting_type][name]["metadata"]["count"][to_add["state"]] += 1
-            global_tree[sorting_type][name]["metadata"]["count"]["total"] += 1
+            global_tree_recv[sorting_type][name]["tests"].append(to_add["test_data"])
+            global_tree_recv[sorting_type][name]["metadata"]["count"][to_add["state"]] += 1
+            global_tree_recv[sorting_type][name]["metadata"]["count"]["total"] += 1
 
     @app.errorhandler(404)
     def page_not_found(e):
