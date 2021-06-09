b386316e89f845b94dadf0ec2df5a9f2eadb0bd0
Paul Canat <pcanat@paratools.fr>
2021-06-09 16:00:23 +0200

    FLASK: cleaning code + loading main page as current state (previously initialized at 0/0/0/0)

--- pcvs/helpers/communications.py
+++ pcvs/helpers/communications.py
@@ -15,6 +15,7 @@ class GenericServer:
     def __init__(self):
         self.labels=[]
         self.tags = []
+        self.waiting = []
         self.metadata = {
             "rootdir": "remote server",
             "count": {
@@ -45,13 +46,22 @@ class EmbeddedServer(GenericServer):
         pass
 class RemoteServer(GenericServer):
     def send(self, label, test):
-        if("label" in test["id"] and test["id"]["label"] not in self.labels):
-            self.labels.append(test["id"]["label"])
-            self.metadata["count"]["labels"] = len(self.labels)
-        if("tags" in test["id"] and test["id"]["tags"] not in self.tags):
-            self.labels.append(test["id"]["label"])
-            self.metadata["count"]["labels"] = len(self.tags)
         self.metadata["count"]["tests"] += 1
+        self.addLabel(test)
+        self.addTags(test)
+        if self._send(label, test):
+            self.sendWaiting()
+        else:
+            self.waiting.append((label, test))
+
+    def sendWaiting(self):
+        while len(self.waiting) > 0:
+            prev_test = self.waiting.pop()
+            if not self._send(prev_test[0], prev_test[1]):
+                self.waiting.append(prev_test[0], prev_test[1])
+
+
+    def _send(self, label, test):
         to_send = {"metadata": self.metadata,
                    "test_data": test,
                    "state": label}
@@ -60,6 +70,16 @@ class RemoteServer(GenericServer):
             return True
         except Exception:
             return False
+
+    def addTags(self, test):
+        if("tags" in test["id"] and test["id"]["tags"] not in self.tags):
+            self.labels.append(test["id"]["label"])
+            self.metadata["count"]["tags"] = len(self.tags)
+
+    def addLabel(self, test):
+        if("label" in test["id"] and test["id"]["label"] not in self.labels):
+            self.labels.append(test["id"]["label"])
+            self.metadata["count"]["labels"] = len(self.labels)
     
     def recv(self):
         return requests.get("http://localhost:5000/submit")
