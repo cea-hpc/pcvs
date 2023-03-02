4009a7f55e60efda5316470e28332b088ca6e683
Julien Adam <adamj@paratools.com>
2023-03-02 14:49:38 +0100

    CONTRIB: add multi jobs per alloc scheduler

--- contrib/multijobs.py
+++ contrib/multijobs.py
@@ -1,26 +1,59 @@
 from pcvs.helpers import log
 from pcvs.orchestration.set import Set
 from pcvs.plugins import Plugin
+from pcvs.testing.test import Test
 
+class SchedMultiJobs(Plugin):
+    step = Plugin.Step.SCHED_SET_EVAL
 
-class PickRandomJobs(Plugin):
-    step = Plugin.Step.INVALID
+    first_run_with_compilation = True
 
-    def run(self, *args, **kwargs):
-        joblist = []
-        jobman = kwargs['jobman']
-
+    def first_allocation(self, jobman):
+        the_set = None
         for dim in range(jobman.nb_dims, 0, -1):
             per_res_list = jobman.get_dim(dim)
-            job = per_res_list.pop()
+            list_count = len(per_res_list)
+            # avoid iterating & push/pop from the same list
+            while list_count > 0:
+                job = per_res_list.pop(0)
+                list_count -= 1
+                if "compilation" in job.tags:
+                    if not the_set:
+                        the_set = Set(execmode=Set.ExecMode.REMOTE)
+                    the_set.add(job)
+                    job.pick()
+                else:
+                    job.not_picked()
+                    per_res_list.append(job)
+        return the_set
 
-            #TODO:
-            joblist.append(job)
-            break
+    def run(self, *args, **kwargs):
+        jobman = kwargs['jobman']
 
-        if len(joblist) > 0:
-            s = Set()
-            s.add(joblist)
-            return s
-
-        return None
+        if self.first_run_with_compilation:
+            self.first_run_with_compilation = not self.first_run_with_compilation
+            return self.first_allocation(jobman)
+        else:
+            the_set = None
+            count = 0
+            for dim in range(jobman.nb_dims, 0, -1):
+                per_res_list = jobman.get_dim(dim)
+                list_count = len(per_res_list)
+                # avoid iterating & push/pop from the same list
+                while list_count > 0:
+                    job = per_res_list.pop(0)
+                    list_count -= 1
+                    if job.has_completed_deps():
+                        if not the_set:
+                            the_set = Set(execmode=Set.ExecMode.ALLOC)
+                        the_set.add(job)
+                        job.pick()
+                        count += 1
+                        if count >= 10:
+                            break
+                    else:
+                        if job.not_picked():
+                            jobman.publish_failed_to_run_job(job, Test.MAXATTEMPTS_STR, Test.State.ERR_OTHER)
+                        else:
+                            per_res_list.append(job)
+            return the_set
