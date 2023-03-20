47714b75ca4166ab67d25be010b267cf1d84e267
Julien Adam <adamj@paratools.com>
2023-03-20 10:28:32 +0100

    MISC: fix & simplify template checker

--- utils/check_valid_templates.py
+++ utils/check_valid_templates.py
@@ -4,48 +4,40 @@ import sys
 import jsonschema
 from ruamel.yaml import YAML, YAMLError
 
-if __name__ == '__main__':
-    ret = 0
-    template_dir = os.path.join(os.path.dirname(__file__), "..", "pcvs", "templates")
-    scheme_dir = os.path.join(os.path.dirname(__file__), "..", "pcvs", "schemes")
-    
-    list_of_templates = list()
-    for s in ['config', 'profile']:
-        d = os.path.join(template_dir, s)
-        for f in os.listdir(d):
-            list_of_templates.append(os.path.join(d, f))
+from pcvs.backend.config import ConfigurationBlock, init as cinit
+from pcvs.backend.profile import Profile, init as pinit
+from pcvs.helpers.exceptions import ValidationException
 
-    list_of_schemes = os.listdir(scheme_dir)
-    verbose = ("-v" in sys.argv)
-    
-    for t in list_of_templates:
-        with open(os.path.join(t), 'r') as fh:
-            template_yaml = YAML(typ='safe').load(fh)
-            err = None
-            state = "NOK"
-            for s in list_of_schemes:
+verbose = ("-v" in sys.argv)
+
+def manage_validation(label, f):
+    err = None
+    try:
+        f()
+        state = "OK"
+    except (ValidationException.FormatError, ValidationException.SchemeError) as e:
+        state = "NOK"
+        err = e.dbg
+    print("* {:12}: {}".format(state, label))
+    if err and verbose:
+        print("\t-> {}".format(err))
         
-                scheme_yaml = {}
-                template_yaml = {}
-                try:
-                    with open(os.path.join(scheme_dir, s), 'r') as fh:
-                        scheme_yaml = YAML(typ='safe').load(fh)
-                    
-                    # ... and validate
-                    jsonschema.validate(template_yaml, scheme_yaml)
-                    state = "OK"
-                    break
-                    
-                # error while loading YAML (either one)
-                except (YAMLError, jsonschema.ValidationError, jsonschema.SchemaError) as e:
-                    state = "NOK"
-                    err = e
-                    
-                # display status
-            print("* {:12}: {}".format(state, t))
-            if err and verbose:
-                print("\t-> {}".format(err))
 
-# exit non-zero if at least one error is raised
-sys.exit(ret)
-    
+if __name__ == '__main__':
+    template_dir = os.path.join(os.path.dirname(__file__), "..", "pcvs", "templates")
+    cinit()
+    for config in os.listdir(os.path.join(template_dir, "config")):
+        conf_kind = config.split(".")[0]
+        t = ConfigurationBlock(conf_kind, "test", "local")
+        with open(os.path.join(template_dir, "config", config), 'r') as fh:
+            data = YAML(typ='safe').load(fh)
+            t.fill(data)
+        manage_validation(config, t.check)
+     
+    pinit()    
+    for profile in os.listdir(os.path.join(template_dir, "profile")):
+        t = Profile("tmp", "local")
+        with open(os.path.join(template_dir, "profile", profile), 'r') as fh:
+            data = YAML(typ='safe').load(fh)
+            t.fill(data)
+        manage_validation(profile, t.check)
